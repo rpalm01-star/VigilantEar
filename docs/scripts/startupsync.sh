1b13088f2fb7c3997a61dfddfe89d5cef2c8b664
rpalm01-star <rpalm01@gmail.com>
2026-03-28 14:29:06 -0700

    Update startupsync.sh

--- docs/scripts/startupsync.sh
+++ docs/scripts/startupsync.sh
@@ -1,7 +1,3 @@
-# 1. CLONE THE PROJECT
-git clone git@github.com:rpalm01-star/VigilantEar.git
-cd VigilantEar
-
 # 2. INSTALL GOOGLE CLOUD SDK
 # If you haven't installed Homebrew yet:
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -14,3 +10,17 @@ gcloud config set project vigilantear-research
 # 4. SETUP XCODE ENVIRONMENT
 # Open the project in Xcode 26
 open com.rpalm.vigilantEar/VigilantEar.xcodeproj
+
+# 5. Generate a new SSH key
+ssh-keygen -t ed25519 -C "robert@wingdingssocial.com"
+
+# 6. Start the ssh-agent and add your key
+eval "$(ssh-agent -s)"
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+
+# 7. Display the key to copy into GitLab (Settings > SSH Keys)
+cat ~/.ssh/id_ed25519.pub
+
+# 8. Clone the repository
+git clone git@github.com:rpalm01-star/VigilantEar.git
+cd VigilantEar
